a699595cd899ea3321095391ddbc6baebef433dd
Maxim Kuznetsov <m.kuznetsov@superjob.ru>
2019-06-14 10:22:38 +0300

    remove nslog

--- FBSnapshotTestCase/FBSnapshotTestController.m
+++ FBSnapshotTestCase/FBSnapshotTestController.m
@@ -223,10 +223,6 @@ typedef NS_ENUM(NSUInteger, FBTestSnapshotFileNameType) {
         return NO;
     }
 
-//    NSLog(@"If you have Kaleidoscope installed you can run this command to see an image diff:\n"
-//          @"ksdiff \"%@\" \"%@\"",
-//          referencePath, testPath);
-
     return YES;
 }
 
@@ -303,7 +299,6 @@ typedef NS_ENUM(NSUInteger, FBTestSnapshotFileNameType) {
         if (!imagesSame) {
             NSError *saveError = nil;
             if ([self saveFailedReferenceImage:referenceImage testImage:snapshot selector:selector identifier:identifier error:&saveError] == NO) {
-//                NSLog(@"Error saving test images: %@", saveError);
             }
         }
         return imagesSame;
@@ -343,7 +338,6 @@ typedef NS_ENUM(NSUInteger, FBTestSnapshotFileNameType) {
             }
             didWrite = [pngData writeToFile:filePath options:NSDataWritingAtomic error:errorPtr];
             if (didWrite) {
-//                NSLog(@"Reference image save at: %@", filePath);
             }
         } else {
             if (nil != errorPtr) {
